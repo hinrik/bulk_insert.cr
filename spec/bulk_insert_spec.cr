19491f0eb94397e267efa184f245285e61024a9c
Hinrik Örn Sigurðsson <hinrik.sig@gmail.com>
2017-11-13 11:10:28 +0100

    Play well with transactions

--- spec/bulk_insert_spec.cr
+++ spec/bulk_insert_spec.cr
@@ -1,4 +1,5 @@
 require "./spec_helper"
+require "db"
 require "sqlite3"
 
 describe BulkInsert do
@@ -7,25 +8,25 @@ describe BulkInsert do
     db.exec "CREATE TABLE test (foo string, bar integer)"
 
     sql = "INSERT INTO test (foo, bar) VALUES"
-    bulk = BulkInsert.new(db, 2, sql, max_args: 999)
+    bulk = BulkInsert.new(2, sql, max_args: 999)
 
     row = ["quux", 1]
-    result = bulk.exec(row)
+    result = bulk.exec(db, row)
     result.should be_a(DB::ExecResult)
     result.rows_affected.should eq(1)
 
-    db.exec "BEGIN"
-    rows = (1..1000).map { ["quux", 1] }
-    rows << ["sfdsf", 9]
-    processed, affected = 0, 0
-    bulk.exec_many(rows) do |nr_rows, result|
-      processed += nr_rows
-      result.should be_a(DB::ExecResult)
-      affected += result.rows_affected
+    db.transaction do |tx|
+      rows = (1..1000).map { ["quux", 1] }
+      rows << ["sfdsf", 9]
+      processed, affected = 0, 0
+      bulk.exec_many(tx.connection, rows) do |nr_rows, result|
+        processed += nr_rows
+        result.should be_a(DB::ExecResult)
+        affected += result.rows_affected
+      end
+      processed.should eq(rows.size)
+      affected.should eq(rows.size)
     end
-    processed.should eq(rows.size)
-    affected.should eq(rows.size)
-    db.exec "COMMIT"
 
     total = db.scalar "SELECT COUNT(*) FROM test"
     total.should eq(1002)
--- src/bulk_insert.cr
+++ src/bulk_insert.cr
@@ -1,46 +1,44 @@
-require "db"
-
 class BulkInsert
   VERSION = "0.1.0"
 
   alias NrRows = Int32
-  @statements = {} of NrRows => DB::PoolStatement
+  @statements = {} of NrRows => String
   DEFAULT_MAX_ARGS = 999 # SQLite's maximum. Pg and MySQL have higher limits
 
   # Example:
   #
-  #   BulkInsert.new(db, 2, "INSERT INTO table (col1, col2) VALUES")
-  def initialize(db, nr_columns, sql_prefix, sql_suffix = "", max_args = DEFAULT_MAX_ARGS)
+  #   BulkInsert.new(2, "INSERT INTO table (col1, col2) VALUES")
+  def initialize(nr_columns, sql_prefix, sql_suffix = "", max_args = DEFAULT_MAX_ARGS)
     template = "(#{ (1..nr_columns).map { %[?] }.join(%[,]) })"
 
     nr_args = max_args - max_args % nr_columns
     while nr_args >= nr_columns
       nr_rows = nr_args / nr_columns
       sql = "#{sql_prefix} #{ (1..nr_rows).map { template }.join(%[, ]) } #{sql_suffix}"
-      @statements[nr_rows] = db.build sql
+      @statements[nr_rows] = sql
       nr_args /= 2
       nr_args -= nr_args % nr_columns
     end
   end
 
-  def exec(row)
-    @statements[1].exec(row)
+  def exec(db, row)
+    db.exec @statements[1], row
   end
 
   # Example:
   #
   #   [["foo", 4], ["bar", 5]]
-  #   lk.exec_many(rows) do |nr_rows, result|
+  #   bulk.exec_many(db, rows) do |nr_rows, result|
   #     update_progress(nr_rows)
   #   end
-  def exec_many(rows)
+  def exec_many(db, rows)
     remaining = rows.size
 
     all_nr_rows = @statements.keys
     i = 0
     while remaining > 0
       nr_rows = all_nr_rows[i]
-      statement = @statements[nr_rows]
+      sql = @statements[nr_rows]
       if nr_rows > remaining
         i += 1
         next
@@ -48,13 +46,13 @@ class BulkInsert
 
       position = rows.size - remaining
       args = rows[position...position+nr_rows].flatten
-      yield nr_rows, statement.exec(args)
+      yield nr_rows, db.exec(sql, args)
       remaining -= nr_rows
     end
   end
 
-  def exec_many(rows)
-    exec_many(rows) {}
+  def exec_many(db, rows)
+    exec_many(db, rows) {}
   end
 end
 
